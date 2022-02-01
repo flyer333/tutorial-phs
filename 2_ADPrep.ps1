64b1c048566aa702626a1ca4a781bb5c681f6e3c
flyer333 <90836515+flyer333@users.noreply.github.com>
2022-02-01 12:26:18 +0100

    Changed Server parameters to match VBox Test Platform
    It does not touch network/computer name

--- 2_ADPrep.ps1
+++ 2_ADPrep.ps1
@@ -14,24 +14,24 @@
 #
 #
 #Declare variables
-$ipaddress = "10.0.1.117" 
+$ipaddress = "10.10.10.10" 
 $ipprefix = "24" 
-$ipgw = "10.0.1.1" 
-$ipdns = "10.0.1.117"
+$ipgw = "10.10.10.1" 
+$ipdns = "10.10.10.10"
 $ipdns2 = "8.8.8.8" 
 $ipif = (Get-NetAdapter).ifIndex 
 $featureLogPath = "c:\poshlog\featurelog.txt" 
-$newname = "DC1"
+$newname = "RWDCESP01"
 $addsTools = "RSAT-AD-Tools" 
 
 #Set static IP address
-New-NetIPAddress -IPAddress $ipaddress -PrefixLength $ipprefix -InterfaceIndex $ipif -DefaultGateway $ipgw 
+#New-NetIPAddress -IPAddress $ipaddress -PrefixLength $ipprefix -InterfaceIndex $ipif -DefaultGateway $ipgw 
 
 #Set the DNS servers
-Set-DnsClientServerAddress -InterfaceIndex $ipif -ServerAddresses ($ipdns, $ipdns2)
+#Set-DnsClientServerAddress -InterfaceIndex $ipif -ServerAddresses ($ipdns, $ipdns2)
 
 #Rename the computer 
-Rename-Computer -NewName $newname -force 
+#Rename-Computer -NewName $newname -force 
 
 #Install features 
 New-Item $featureLogPath -ItemType file -Force 
